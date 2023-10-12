6b01c9ec1c0ee72b7baf687c58b145a6eafac053
Mahesh Verma <109792015+MaheshVerma12@users.noreply.github.com>
2023-10-13 00:53:24 +0545

    Add files via upload

---
+++ hey.java
@@ -0,0 +1,8 @@
+public class hey
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hi there good souls.");
+        System.out.println("Remain good people.");
+    }
+}
